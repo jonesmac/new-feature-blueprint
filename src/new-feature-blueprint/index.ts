727a164c1183c28fe7ab857bf4c1a26296ef1aa8
Matt <jones@railroad19.com>
2018-09-14 11:47:03 -0400

    add dynamic file name and chaining

--- src/new-feature-blueprint/index.ts
+++ src/new-feature-blueprint/index.ts
@@ -1,10 +1,11 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics';
 // import { testRule } from './test-rule';
 
 export function newFeatureBlueprint(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    // testRule(null);
-    tree.create('src/app/app.module.ts', 'file contents');
-    return tree;
-  };
+  return chain([
+    (tree: Tree, _context: SchematicContext) => {
+      tree.create(`src/app/test/${_options.name || 'myfile'}.ts`, 'some content');
+      return tree
+    }
+  ]);
 }
